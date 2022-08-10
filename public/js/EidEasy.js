e61f81440ed5cfd4a775af6ccedc97a798505d47
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-08-10 18:04:32 +0300

    fix: improve iframe styles

--- public/js/EidEasy.js
+++ public/js/EidEasy.js
@@ -14,8 +14,20 @@ function () {
     this.baseUrl = baseUrl;
   }
 
+  EidEasy.prototype.createOverlay = function (src) {
+    var div = document.createElement('div');
+    div.innerHTML = "<div id=\"overlay\" style=\"z-index: 9999999; width: 100vw; height: 100%; overflow: auto; background: rgba(0, 0, 0, 0.6); position: fixed !important; inset: 0px !important; display: block !important; margin: 0px !important; padding: 0px !important;\"><iframe src=\"".concat(src, "\" allow=\"autoplay; camera; microphone\" id=\"veriffFrame\" style=\"width: 100vw; max-width: 600px; height: 700px;  background: none; border: none; position: absolute !important; inset: 0px !important; margin: 30px auto !important; padding: 0px !important;\"></iframe></div>").trim();
+    return div.firstChild;
+  };
+
   EidEasy.prototype.start = function (clientId, docId, actionType, country) {
-    window.location.href = "".concat(this.baseUrl, "/single-method-signature?client_id=").concat(clientId, "&doc_id=").concat(docId, "&method=").concat(actionType, "&country=").concat(country);
+    var iframeSrc = "".concat(this.baseUrl, "/single-method-signature?client_id=").concat(clientId, "&doc_id=").concat(docId, "&method=").concat(actionType, "&country=").concat(country);
+    var overlay = this.createOverlay(iframeSrc);
+    document.body.appendChild(overlay);
+    overlay.addEventListener('click', function (e) {
+      e.preventDefault();
+      overlay.remove();
+    });
   };
 
   return EidEasy;
--- resources/js/EidEasy.ts
+++ resources/js/EidEasy.ts
@@ -5,8 +5,20 @@ class EidEasy {
     this.baseUrl = baseUrl;
   }
 
+  private createOverlay(src: string) {
+    const div = document.createElement('div');
+    div.innerHTML = `<div id="overlay" style="z-index: 9999999; width: 100vw; height: 100%; overflow: auto; background: rgba(0, 0, 0, 0.6); position: fixed !important; inset: 0px !important; display: block !important; margin: 0px !important; padding: 0px !important;"><iframe src="${src}" allow="autoplay; camera; microphone" id="veriffFrame" style="width: 100vw; max-width: 600px; height: 700px;  background: none; border: none; position: absolute !important; inset: 0px !important; margin: 30px auto !important; padding: 0px !important;"></iframe></div>`.trim();
+    return div.firstChild;
+  }
+
   start(clientId: string, docId: string, actionType: string, country: string) {
-    window.location.href = `${this.baseUrl}/single-method-signature?client_id=${clientId}&doc_id=${docId}&method=${actionType}&country=${country}`;
+    const iframeSrc = `${this.baseUrl}/single-method-signature?client_id=${clientId}&doc_id=${docId}&method=${actionType}&country=${country}`;
+    const overlay = this.createOverlay(iframeSrc);
+    document.body.appendChild(overlay);
+    overlay.addEventListener('click', (e) => {
+      e.preventDefault();
+      overlay.remove();
+    });
   }
 }
 
