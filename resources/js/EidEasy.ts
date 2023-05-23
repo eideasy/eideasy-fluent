a8e2d13c797877336935020a9bdd0b8ff3c56f51
Mats-Joonas Kulla <matsjoonas@gmail.com>
2023-05-23 15:47:03 +0300

    Listen for single method events

--- resources/js/EidEasy.ts
+++ resources/js/EidEasy.ts
@@ -2,25 +2,65 @@ import windowOpen from "./windowOpen";
 
 class EidEasy {
   baseUrl: string;
+  openedWindow: Window;
+  onSuccess: Function;
+  onFail: Function;
 
-  constructor(baseUrl: string = "https://id.eideasy.com") {
+  constructor(
+    baseUrl: string = "https://id.eideasy.com",
+    onSuccess: Function = () => {},
+    onFail: Function = () => {},
+  ) {
     this.baseUrl = baseUrl;
+    this.onSuccess = onSuccess;
+    this.onFail = onFail;
   }
 
-  private createOverlay(src: string) {
-    const div = document.createElement('div');
-    div.innerHTML = `<div id="overlay" style="z-index: 9999999; width: 100vw; height: 100%; overflow: auto; background: rgba(0, 0, 0, 0.6); position: fixed !important; inset: 0px !important; display: block !important; margin: 0px !important; padding: 0px !important;"><iframe src="${src}" allow="autoplay; camera; microphone" id="veriffFrame" style="width: 100vw; max-width: 600px; height: 700px;  background: none; border: none; position: absolute !important; inset: 0px !important; margin: 30px auto !important; padding: 0px !important;"></iframe></div>`.trim();
-    return div.firstChild;
-  }
-
-  start(clientId: string, docId: string, actionType: string, country: string) {
+  start({
+    clientId,
+    docId,
+    actionType,
+    country
+  }) {
+    const _self = this;
     const url = `${this.baseUrl}/single-method-signature?client_id=${clientId}&doc_id=${docId}&method=${actionType}&country=${country}`;
-    windowOpen({
+    const windowOpenResult = windowOpen({
       url,
       onClosed: () => {
         console.log('closed');
       }
-    })
+    });
+
+    this.openedWindow = windowOpenResult.window;
+
+    window.addEventListener('message', (event) => {
+      console.log('message received');
+      console.log(event);
+      const {data} = event;
+
+      if (data.sender !== 'EIDEASY_SINGLE_METHOD_SIGNATURE') {
+        return;
+      }
+
+      if (data.type === 'SUCCESS') {
+        _self.handleSuccess(data.result);
+      } else if (data.type === 'FAIL') {
+        _self.handleFail(data.error);
+      }
+    });
+  }
+
+  handleSuccess(result) {
+    console.log('handleSuccess in EidEasy.ts');
+    console.log(result);
+    this.openedWindow.close();
+    this.onSuccess(result);
+  }
+
+  handleFail(error) {
+    console.log('handleFail in EidEasy.ts');
+    console.log(error);
+    this.onFail(error);
   }
 }
 
